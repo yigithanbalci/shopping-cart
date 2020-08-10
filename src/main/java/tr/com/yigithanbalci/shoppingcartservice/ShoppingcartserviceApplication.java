adc531dd3b0bb6abe73c881835026fe6a84ff4fa
yigithanbalci <yigithanbalci@gmail.com>
2020-08-10 21:34:22 +0300

    Initial commit.

---
+++ src/main/java/tr/com/yigithanbalci/shoppingcartservice/ShoppingcartserviceApplication.java
@@ -0,0 +1,13 @@
+package tr.com.yigithanbalci.shoppingcartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/com/yigithanbalci/shoppingcartservice/ShoppingcartserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.yigithanbalci.shoppingcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

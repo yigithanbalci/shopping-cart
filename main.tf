dad493487b9cd14c4de31d61424093bd9a7bceb2
Yigithan Kadir BALCI <yigithanbalci@gmail.com>
2020-08-24 16:07:02 +0300

    Updated main.tf because of misuse of memory unnecessarily large.

--- main.tf
+++ main.tf
@@ -42,14 +42,14 @@ resource "aws_ecs_task_definition" "shopping-cart-task" {
           "hostPort": 8080
         }
       ],
-      "memory": 1024,
+      "memory": 512,
       "cpu": 256
     }
   ]
   DEFINITION
   requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-  memory                   = 1024         # Specifying the memory our container requires
+  memory                   = 512         # Specifying the memory our container requires
   cpu                      = 256         # Specifying the CPU our container requires
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
